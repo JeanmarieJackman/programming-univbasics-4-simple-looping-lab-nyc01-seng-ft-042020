ec17cbfd941532c10a9ba9fb6d2bf03302df8488
Jeanmarie Jackman <jeanmarie.jackman@gmail.com>
2020-04-11 03:02:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,3 +13,11 @@ def loop_message_n_times(message, limit)
     count += 1 
   end
 end
+
+def loop_message_n_times(message, limit)
+  count = 0
+  while count < limit do
+    puts message 
+    count += 1 
+  end
+end
