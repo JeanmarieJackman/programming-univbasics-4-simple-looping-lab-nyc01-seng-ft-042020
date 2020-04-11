0363608d66dd3bd37208217d37b8949cfba7d6f1
Jeanmarie Jackman <jeanmarie.jackman@gmail.com>
2020-04-11 03:01:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,14 +1,14 @@
 def loop_message_five_times(message)
   count = 0
-  while count < 5
+  while count < 5 do
     puts message 
     count += 1 
   end
 end
 
-def loop_message_five_times(message)
+def loop_message_n_times(message, limit)
   count = 0
-  while count < 5
+  while count < limit do
     puts message 
     count += 1 
   end
